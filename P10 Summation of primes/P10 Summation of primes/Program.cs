8cfbb8c836a3b799ef8c5001ab7a4f1aec0ef224
BreadButterCat <40633228+tulza@users.noreply.github.com>
2022-05-24 22:53:54 +1200

    Solved Problem 10

---
+++ P10 Summation of primes/P10 Summation of primes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace P10_Summation_of_primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Summation of primes (Problem 10)
+            //https://projecteuler.net/problem=10
+
+            //The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+            //the sum of all the primes below two million.
+
+            //summery
+            //find the sum of all prime under 2 million
+
+            long sum = 0, max = 2000000; //2 million
+
+            //start at 2 add by one then + 2 onward as even number has 2 as a mutiple
+            for (long i = 2; i < max; i+=2)
+            {
+                if (i == 2)
+                {
+                    sum += 2;
+                    i++;
+                }
+
+                if (IsPrime(i))
+                {
+                    //checking and summing up
+                    Console.WriteLine($"{i} {sum}");
+                    sum += i;
+                }
+            }
+            Console.WriteLine("the sum is!");
+            Console.WriteLine(sum);
+            //result:
+            //142913828922 (correct) 24/05/22
+
+            //method of finding if prime
+            static bool IsPrime(long num)
+            {
+                for (int i = 3; i < (int)num/2; i+=2)
+                {
+                    if (num%i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -17,3 +17,4 @@ Repository of me trying to solve and optimise project euler's problems
 	P7) 10001st prime - solved(24/05/22) - Diff 5%						https://projecteuler.net/problem=7 
 	P8) largest product in a series - solved(24/05/22) - Diff 5%        https://projecteuler.net/problem=8   
 	P9) Special Pythagorean triplet - solved(24/05/22) -Diff 5%			https://projecteuler.net/problem=9
+	P10) Summation of primes - solved(24/05/22) -Diff 5%				https://projecteuler.net/problem=10
